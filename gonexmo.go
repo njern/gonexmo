9041a232d98afd3cca90cece850fde78add339a3
Niclas Jern <niclas@walkbase.com>
2013-12-25 23:29:08 +0200

    Remove extraneous newline before package declaration.

--- gonexmo.go
+++ gonexmo.go
@@ -6,7 +6,6 @@ your API key and API secret. Then send messages with SendTextMessage() or
 SendFlashMessage(). The API will return a MessageResponse which you can
 use to see if your message went through, how much it cost, etc.
 */
-
 package gonexmo
 
 import (
