7a51cbac174b8bafd0484e796d1827267d580446
Niclas Jern <niclas@walkbase.com>
2013-12-25 23:02:56 +0200

    Added package comment.

--- gonexmo.go
+++ gonexmo.go
@@ -1,3 +1,12 @@
+/*
+Package gonexmo implements a simple client library for accessing the Nexmo API.
+
+Usage is simple. Create a Nexmo instance with NexmoWithKeyAndSecret(), providing
+your API key and API secret. Then send messages with SendTextMessage() or
+SendFlashMessage(). The API will return a MessageResponse which you can
+use to see if your message went through, how much it cost, etc.
+*/
+
 package gonexmo
 
 import (
