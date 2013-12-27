617623561f48af404d8d3e71df24d765e88a9f48
inhies <inhies@gmail.com>
2013-12-26 14:00:27 -1000

    Use enviroment variables to store testing credentials
    Instead of forcing users to edit the file and include their API
    credentials, where they could potentially end up checked in to their git
    repository and/or end up posted on Github, use enviroment variables to
    store them.
    
    They can be set globally by exporting them, then calling the program, or
    you can pass them to `go test`:
    
    NEXMO_KEY=something NEXMO_SECRET=something NEXMO_NUM=18005555555 go test

--- gonexmo_test.go
+++ gonexmo_test.go
@@ -1,14 +1,34 @@
 package nexmo
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
-const (
-	API_KEY           = "YOUR NEXMO API KEY GOES HERE"
-	API_SECRET        = "YOUR NEXMO API SECRET GOES HERE"
-	TEST_PHONE_NUMBER = "YOUR PHONE NUMBER GOES HERE"
+var (
+	API_KEY, API_SECRET, TEST_PHONE_NUMBER string
 )
+
+func init() {
+	API_KEY = os.Getenv("NEXMO_KEY")
+	if API_KEY == "" {
+		fmt.Println("No API key specified. Please set NEXMO_KEY")
+		os.Exit(1)
+	}
+
+	API_SECRET = os.Getenv("NEXMO_SECRET")
+	if API_SECRET == "" {
+		fmt.Println("No API secret specified. Please set NEXMO_SECRET")
+		os.Exit(1)
+	}
+
+	TEST_PHONE_NUMBER = os.Getenv("NEXMO_NUM")
+	if TEST_PHONE_NUMBER == "" {
+		fmt.Println("No test phone number specified. Please set NEXMO_NUM")
+		os.Exit(1)
+	}
+}
 
 func TestNexmoCreation(t *testing.T) {
 	_, err := NexmoWithKeyAndSecret(API_KEY, API_SECRET)
